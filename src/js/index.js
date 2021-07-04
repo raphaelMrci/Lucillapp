a7f882cadef262669154f285dd0a4a79d77f80ab
Raphael <raphsurvenus@gmail.com>
2021-07-04 03:45:04 +0200

    Ingredients implementation
    The ingredient page was created
    
    //WIP: Edit overlay

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const ingredientBtn = document.getElementById( "ingredients" );
+
+ingredientBtn.style.cursor = 'pointer';
+
+ingredientBtn.onclick = () => {
+    window.location.href = "../html/ingredients.html"
+}
---
+++ src/js/navbar.js
@@ -0,0 +1,22 @@
+const ingredientBtn = document.getElementById( 'ingredients_menu' );
+const recipesBtn = document.getElementById( 'recipes_menu' );
+const calculateBtn = document.getElementById( 'calculate_menu' );
+
+const selector = document.getElementById( 'nav_selector' );
+
+
+ingredientBtn.style.cursor = 'pointer';
+recipesBtn.style.cursor = 'pointer';
+calculateBtn.style.cursor = 'pointer';
+
+ingredientBtn.onclick = () => {
+    window.location.href = '../html/ingredients.html';
+}
+
+recipesBtn.onclick = () => {
+    window.location.href = '../html/recipes.html';
+}
+
+calculateBtn.onclick = () => {
+    window.location.href = '../html/calculate.html';
+}
